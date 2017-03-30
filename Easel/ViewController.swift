614a0f32fabd599f3f201730fea31ad9fd520dda
vlgomes <uvlgomes@gmail.com>
2017-03-30 12:58:59 +0100

    Drawing Done

--- Easel/ViewController.swift
+++ Easel/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
     @IBOutlet var buttonsStackView: UIStackView!
     
-    var moveCount = 0
+    var lastPoint = CGPoint.zero
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,13 +30,36 @@ class ViewController: UIViewController {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("Began")
         self.buttonsStackView.isHidden = true
+        
+        if let touch =  touches.first{
+            let point = touch.location(in: self.imageView)
+            
+            self.lastPoint = point
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("Moved: \(self.moveCount)")
-        moveCount += 1
+        
+        if let touch =  touches.first{
+            let point = touch.location(in: self.imageView)
+            
+            //To draw we have to begin and get the context
+            UIGraphicsBeginImageContext(self.imageView.frame.size)
+            let context = UIGraphicsGetCurrentContext()
+            
+            self.imageView.image?.draw(in: CGRect(x: 0, y: 0, width: self.imageView.frame.size.width, height: self.imageView.frame.size.height))
+            
+            context?.move(to: self.lastPoint)
+            context?.addLine(to: point)
+            context?.strokePath()
+            
+            self.imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+            
+            UIGraphicsEndImageContext()
+            
+            self.lastPoint = point
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
